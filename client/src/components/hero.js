e0241640e20572fc87bc0f6dd09f04ec9271db68
Iskandar Kurbanov <iskurbanov@gmail.com>
2019-05-24 13:45:11 -0400

    nav and hero changes

--- client/src/components/hero.js
+++ client/src/components/hero.js
@@ -8,15 +8,15 @@ export default class HeroCarousel extends Component {
 		return(
 			<Carousel showThumbs={false}>
 			                <div>
-			                    <img src={require("../assets/jakob-owens-518866-unsplash.jpg")} />
+			                    <img src={require("../images/hero-image@3x.jpg")} />
 			                    <p className="legend">Legend 1</p>
 			                </div>
 			                <div>
-			                    <img src={require("../assets/jakob-owens-518866-unsplash.jpg")} />
+			                    <img src={require("../images/hero-image@3x.jpg")} />
 			                    <p className="legend">Legend 2</p>
 			                </div>
 			                <div>
-			                    <img src={require("../assets/jakob-owens-518866-unsplash.jpg")} />
+			                    <img src={require("../images/hero-image@3x.jpg")} />
 			                    <p className="legend">Legend 3</p>
 			                </div>
 		    </Carousel>
