88cfe54350a1d3fc8e0fbf9b3697904187e4fbbf
suyangsong <suyangsong@gmail.com>
2019-05-24 12:38:39 -0400

    carousel functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import Carousel from './components/hero.js';
 import './styles/_master.scss';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
     <BrowserRouter>
           <NavBar />
+	  <Carousel />
     </BrowserRouter>
 
     </div>
