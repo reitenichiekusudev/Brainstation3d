d68bbfe15c070f0d81cfdc5444315cca378a8ed5
Iskandar Kurbanov <iskurbanov@gmail.com>
2019-05-24 13:25:18 -0400

    changed font color

---
+++ client/src/components/Images.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+
+
+export class Images extends Component {
+    render() {
+        return (
+            <div>
+                <div className="images__wrapper">
+                    <div className="images__image-container">
+                    
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Images
